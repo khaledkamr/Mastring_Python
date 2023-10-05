f7b6bcadb556b3c0f0cc3f3763094181fb2c03ec
khaledkamr <200013171@must.edu.eg>
2023-10-05 23:06:06 +0300

    Added note about indexing and slicing

--- B - strings.py
+++ B - strings.py
@@ -49,6 +49,15 @@ print(a + "\n" + b)
 
 print("Hello " + 1)  # Error; only concatenate strings
 
+# ---------------------------------
+# Strings Indexing & Slicing
+# [1] All Data in Python is Object
+# [2] Object Contain Elements
+# [3] Every Element Has Its Own Index
+# [4] Python Use Zero Based Indexing ( Index Start From Zero )
+# [5] Use Square Brackets To Access Element
+# [6] Enable Accessing Parts Of Strings, Tuples or Lists
+# ---------------------------------
 
 #indexing (access single item)
 string = "theweeknd"
