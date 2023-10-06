f2382dbcf7cd943874f4d0beaa30d6d68a4a39ac
khaledkamr <200013171@must.edu.eg>
2023-10-06 20:13:30 +0300

    Updated the boolean file

--- H - Boolean.py
+++ H - Boolean.py
@@ -59,3 +59,58 @@ print(age > 40 or country == "Egypt" or rank > 20)  # True
 
 print(age > 16)  # True
 print(not age > 16)  # Not True = False
+
+# --------------------------
+# -- Comparison Operators --
+# --------------------------
+# [ == ] Equal
+# [ != ] Not Equal
+# [ > ] Greater Than
+# [ < ] Less Than
+# [ >= ] Greater Than Or Equal
+# [ <= ] Less Than Or Equal
+# --------------------------
+
+# Equal + Not Equal
+
+print(100 == 100)
+print(100 == 100.00)
+
+print("#" * 50)
+
+print(100 != 100)
+print(100 != 100.00)
+
+print("#" * 50)
+
+# Greater Than + Less Than
+
+print(100 > 100)
+print(100 > 200)
+print(100 > 100.00)
+print(100 > 40)
+
+print("#" * 50)
+
+print(100 < 100)
+print(100 < 200)
+print(100 < 100.00)
+print(100 < 40)
+
+print("#" * 50)
+
+# Greater Than Or Equal + Less Than Or Equal
+
+print(100 >= 100)
+print(100 >= 200)
+print(100 >= 100.00)
+print(100 >= 40)
+
+print("#" * 50)
+
+print(100 <= 100)
+print(100 <= 200)
+print(100 <= 100.00)
+print(100 <= 40)
+
+print("#" * 50)
