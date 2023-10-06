1166eb38219cd3ce1ba92a304b66b089ed833b37
khaledkamr <200013171@must.edu.eg>
2023-10-06 19:57:02 +0300

    Edited osama to khaled :)

--- B - strings.py
+++ B - strings.py
@@ -198,11 +198,11 @@ print(b.find("P", 0, 5))  # -1
 
 # rjust(Width, Fill Char) ljust(Width, Fill Char)
 
-c = "Osama"
+c = "Khaled"
 print(c.rjust(10))
 print(c.rjust(10, "#"))
 
-d = "Osama"
+d = "Khaled"
 print(d.ljust(10))
 print(d.ljust(10, "#"))
 
@@ -238,9 +238,9 @@ six = 'I Love Python'
 print(five.islower())
 print(six.islower())
 
-seven = "osama_elzero"
-eight = "OsamaElzero100"
-nine = "Osama--Elzero100"
+seven = "Khaled_kamr"
+eight = "Khaledkamr99"
+nine = "Khaled--kamr99"
 
 print(seven.isidentifier())
 print(eight.isidentifier())
@@ -265,7 +265,7 @@ print(a.replace("One", "1", 2))
 
 # join(Iterable)
 
-myList = ["Osama", "Mohamed", "Elsayed"]
+myList = ["Khaled", "Mohamed", "Elsayed"]
 print("-".join(myList))
 print(" ".join(myList))
 print(", ".join(myList))
