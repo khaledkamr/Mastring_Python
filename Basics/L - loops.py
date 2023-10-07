832d89ef90ee567d04abe5236efdf6b1dad4aafd
khaledkamr <200013171@must.edu.eg>
2023-10-07 11:25:33 +0300

    sorted the files and megred the break,continue,pass file with loops file

--- Basics/L - loops.py
+++ Basics/L - loops.py
@@ -81,3 +81,44 @@ mySkills = {
 for skill in mySkills:
 
   print(f"My Progress in Lang {skill} Is: {mySkills[skill]}") 
+
+
+# ---------------------------
+# -- Break, Continue, Pass --
+# ---------------------------
+
+myNumbers = [1, 2, 3, 5, 7, 10, 13, 14, 15, 19]
+
+# Continue
+
+for number in myNumbers:
+
+  if number == 13:
+
+    continue
+
+  print(number)
+
+print("#" * 50)
+
+# Break
+
+for number in myNumbers:
+
+  if number == 13:
+
+    break
+
+  print(number)
+
+print("#" * 50)
+
+# Pass
+
+for number in myNumbers:
+
+  if number == 13:
+
+    pass
+
+  print(number)
