2c99e1a4337d84cca314783537a27a370cfd82d9
khaledkamr <200013171@must.edu.eg>
2023-10-11 07:50:52 +0300

    Added the Iterable vs Iterator file

---
+++ Basics/U - Iterable vs Iterator.py
@@ -0,0 +1,38 @@
+# --------------------------
+# -- Iterable vs Iterator --
+# --------------------------
+# Iterable
+# [1] Object Contains Data That Can Be Iterated Upon
+# [2] Examples (String, List, Set, Tuple, Dictionary)
+# ------------------------------------------
+# Iterator
+# [1] Object Used To Iterate Over Iterable Using next() Method Return 1 Element At A Time
+# [2] You Can Generate Iterator From Iterable When Using iter() Method
+# [3] For Loop Already Calls iter() Method on The Iterable Behind The Scene
+# [4] Gives "StopIteration" If Theres No Next Element
+# -----------------------------------------------------------
+
+String = "KHALED"
+List = [1, 2, 3, 4, 5]
+
+for l in String:
+  print(l, end=" ")
+
+for n in List:
+  print(n, end=" ")
+
+# For Loop Already Calls iter() Method
+# the variables 'n' and 'l' are iterators
+#------------------------------------------
+
+Iterator = iter(String)
+
+print(next(Iterator))
+print(next(Iterator))
+print(next(Iterator))
+print(next(Iterator))
+print(next(Iterator))
+print(next(Iterator))
+
+for L in iter("KHALED"):
+  print(L, end=" ")
