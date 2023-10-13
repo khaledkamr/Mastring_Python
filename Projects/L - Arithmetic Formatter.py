fc7dc6579933623898e0dbb436421f40fbfc004b
khaledkamr <200013171@must.edu.eg>
2023-10-13 17:58:34 +0300

    Edited the code

--- Projects/L - Arithmetic Formatter.py
+++ Projects/L - Arithmetic Formatter.py
@@ -7,8 +7,7 @@ def arithmetic_arranger(*problem):
             num1 = p[0 : p.find('+')].strip()
             num2 = p[p.find('+')+1 : ].strip()
             res = int(num1) + int(num2)
-
-        elif (p.find('-') != -1):
+        else:
             num1 = p[0 : p.find('-')].strip()
             num2 = p[p.find('-')+1 : ].strip()
             res = int(num1) - int(num2)
@@ -18,20 +17,17 @@ def arithmetic_arranger(*problem):
         if (len(num1) < len(num2)): # 3 < 1
             print(" " * abs(len(num1) - len(num2)), end = "")
             print(num1)   
-
         else:
             print(num1)
         
         if (p.find('+') != -1):
             print("+ ", end = "")
-
-        elif (p.find('-') != -1):
+        else:
             print("- ", end = "")
 
         if (len(num2) < len(num1)): # 3 < 2
             print(" " * abs(len(num1) - len(num2)), end = "")
             print(num2)
-
         else:
             print(num2)
 
@@ -39,14 +35,13 @@ def arithmetic_arranger(*problem):
 
         res = str(res)
 
-        if len(res) > max(len(num1), len(num2)): # 2 > 3
+        if (len(res) > max(len(num1), len(num2))): # 2 > 3
             print(f" {res}")
         
-        elif len(res) < max(len(num1), len(num2)):
+        elif (len(res) < max(len(num1), len(num2))):
             print("  ", end = "")
             print(" " * abs(len(str(max(len(num1), len(num2)))) - len(res)), end = "")
             print(res)
-
         else:
             print(f"  {res}")
         print("\n")
@@ -62,16 +57,16 @@ mylist = []
 while (n):
     eq = input("enter the problem : ").strip()
 
-    if (eq.find('+') != -1):
-        num1 = eq[0 : eq.find('+')].strip()
-        num2 = eq[eq.find('+')+1 : ].strip()
-    elif (eq.find('-') != -1):
-        num1 = eq[0 : eq.find('-')].strip()
-        num2 = eq[eq.find('-')+1 : ].strip()
-
     while (eq.find('*') != -1 or eq.find('/') != -1):
         print("Operator must be '+' or '-'.")
         eq = input("enter the problem again : ")
+
+    if (eq.find('+') != -1):
+        num1 = eq[0 : eq.find('+')].strip()
+        num2 = eq[eq.find('+')+1 : ].strip()
+    else:
+        num1 = eq[0 : eq.find('-')].strip()
+        num2 = eq[eq.find('-')+1 : ].strip()
 
     while (len(num1) > 4 or len(num2) > 4):
         print("Numbers cannot be more than four digits.")
@@ -79,9 +74,8 @@ while (n):
 
         if (eq.find('+') != -1):
             num1 = eq[0 : eq.find('+')].strip()
-            num2 = eq[eq.find('+')+1 : ].strip()
-       
-        elif (eq.find('-') != -1):
+            num2 = eq[eq.find('+')+1 : ].strip()    
+        else:
             num1 = eq[0 : eq.find('-')].strip()
             num2 = eq[eq.find('-')+1 : ].strip()
     
