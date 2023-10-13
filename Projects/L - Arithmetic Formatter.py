4ba96f04fca44d8e535ec2cbbe3ceb06495b3385
khaledkamr <200013171@must.edu.eg>
2023-10-13 17:48:38 +0300

    Added the subtraction opration to the function

--- Projects/L - Arithmetic Formatter.py
+++ Projects/L - Arithmetic Formatter.py
@@ -2,26 +2,31 @@
 def arithmetic_arranger(*problem):
 
     for p in problem:
+
         if (p.find('+') != -1):
-            num1 = p[0 : p.find('+')]
-            num2 = p[p.find('+')+1 : ]
+            num1 = p[0 : p.find('+')].strip()
+            num2 = p[p.find('+')+1 : ].strip()
             res = int(num1) + int(num2)
 
         elif (p.find('-') != -1):
-            num1 = p[0 : p.find('-')]
-            num2 = p[p.find('-')+1 : ]
+            num1 = p[0 : p.find('-')].strip()
+            num2 = p[p.find('-')+1 : ].strip()
             res = int(num1) - int(num2)
         
         print("  ", end = "")
 
-        if (len(num1) < len(num2)): # 2 < 3
+        if (len(num1) < len(num2)): # 3 < 1
             print(" " * abs(len(num1) - len(num2)), end = "")
             print(num1)   
 
         else:
             print(num1)
         
-        print("+ ", end = "")
+        if (p.find('+') != -1):
+            print("+ ", end = "")
+
+        elif (p.find('-') != -1):
+            print("- ", end = "")
 
         if (len(num2) < len(num1)): # 3 < 2
             print(" " * abs(len(num1) - len(num2)), end = "")
@@ -34,17 +39,22 @@ def arithmetic_arranger(*problem):
 
         res = str(res)
 
-        if len(res) > max(len(num1), len(num2)):
+        if len(res) > max(len(num1), len(num2)): # 2 > 3
             print(f" {res}")
+        
+        elif len(res) < max(len(num1), len(num2)):
+            print("  ", end = "")
+            print(" " * abs(len(str(max(len(num1), len(num2)))) - len(res)), end = "")
+            print(res)
 
         else:
             print(f"  {res}")
-        
+        print("\n")
 
 n = int(input("How many problems you got : (1-5) "))
 
 while n > 5 : 
-    print("sorry, the maximum number of problems is 5!")
+    print("Too many problems.")
     n = int(input("enter the number of problems again : "))
 
 mylist = []
@@ -52,9 +62,43 @@ mylist = []
 while (n):
     eq = input("enter the problem : ").strip()
 
-    while(eq.find('*') != -1 or eq.find('/') != -1):
-        print("only + and - opration is allowed!")
+    if (eq.find('+') != -1):
+        num1 = eq[0 : eq.find('+')].strip()
+        num2 = eq[eq.find('+')+1 : ].strip()
+    elif (eq.find('-') != -1):
+        num1 = eq[0 : eq.find('-')].strip()
+        num2 = eq[eq.find('-')+1 : ].strip()
+
+    while (eq.find('*') != -1 or eq.find('/') != -1):
+        print("Operator must be '+' or '-'.")
         eq = input("enter the problem again : ")
+
+    while (len(num1) > 4 or len(num2) > 4):
+        print("Numbers cannot be more than four digits.")
+        eq = input("enter the problem again : ") 
+
+        if (eq.find('+') != -1):
+            num1 = eq[0 : eq.find('+')].strip()
+            num2 = eq[eq.find('+')+1 : ].strip()
+       
+        elif (eq.find('-') != -1):
+            num1 = eq[0 : eq.find('-')].strip()
+            num2 = eq[eq.find('-')+1 : ].strip()
+    
+    # try:
+    #     int(num1)
+    #     int(num2)
+    # except :
+    #     print("Numbers must only contain digits.")
+    #     eq = input("enter the problem again : ")
+
+    #     if (eq.find('+') != -1):
+    #         num1 = eq[0 : eq.find('+')].strip()
+    #         num2 = eq[eq.find('+')+1 : ].strip()
+       
+    #     elif (eq.find('-') != -1):
+    #         num1 = eq[0 : eq.find('-')].strip()
+    #         num2 = eq[eq.find('-')+1 : ].strip()
 
     mylist.append(eq)
     n -= 1
