9dacf65ce97ade02469a74632934617b914f8c90
khaledkamr <200013171@must.edu.eg>
2023-10-19 22:28:54 +0300

    edited the add_time() function

--- Projects/N - Time calculator.py
+++ Projects/N - Time calculator.py
@@ -1,29 +1,67 @@
 
 def add_time(start_time, duration_time, starting_day = "unknown"):
 
-    S_hour = start_time[0 : start_time.find(':')]
-    if(start_time.find('PM') != -1):
-        S_minute = start_time[start_time.find(':') + 1 : start_time.find('PM')]
+    days = ['saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday']
 
+    if (start_time.find('PM') != -1):
+        S_time = 'PM'
     else:
-        S_minute = start_time[start_time.find(':') + 1 : start_time.find('AM')]
+        S_time = 'AM'
+
+    S_hour = start_time[0 : start_time.find(':')]
+
+    if(S_time == 'PM'):
+        S_minute = start_time[start_time.find(':') + 1 : start_time.find('PM')].strip()
+    else:
+        S_minute = start_time[start_time.find(':') + 1 : start_time.find('AM')].strip()
+
     D_hour = duration_time[0 : duration_time.find(':')]
-    D_minute = duration_time[duration_time.find(':') + 1 : ]
+
+    if (duration_time.find('PM') != -1):
+        D_time = 'PM'
+    else:
+        D_time = 'AM'
+
+    if(D_time == 'PM'):
+        D_minute = duration_time[duration_time.find(':') + 1 : duration_time.find('PM')].strip()
+    else:
+        D_minute = duration_time[duration_time.find(':') + 1 : duration_time.find('AM')].strip()
 
     hours = abs(int(S_hour) + int(D_hour))
-    if(hours > 12):
+
+    while(hours > 12):
         hours -= 12
+        if (S_time == 'PM'):
+            S_time = 'AM'
+
+            if(starting_day != "unknown"):
+                d = days.index(starting_day)
+                starting_day = days[d + 1]
+        else:
+           S_time = "PM"
+        
     minutes = abs(int(S_minute) + int(D_minute))
-    if(minutes > 59):
-        pass
 
-    print(f"{hours}:{minutes}")
+    while(minutes > 59):
+        minutes -= 60
+        hours += 1
+        if(hours > 12):
+            hours -= 12
+            if (S_time == 'PM'):
+                S_time = 'AM'
 
+                if(starting_day != "unknown"):
+                    d = days.index(starting_day)
+                    starting_day = days[d + 1]
+            else:
+                S_time = "PM"
 
+    print(f"{hours}:{minutes} {S_time} and day: {starting_day}")
 
 start_time = input("Enter the starting time : ")
 duration_time = input("Enter the duration time : ")
 yesOrNo = input("would you like to add a starting day? (yes or no)")
+
 if(yesOrNo == "yes"):
     starting_day = input("Enter the startig day : ")
     add_time(start_time, duration_time, starting_day)
