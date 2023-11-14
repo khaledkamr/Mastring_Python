f35416a9daa3106a64e1a3335c124a31dcceca6f
khaledkamr <200013171@must.edu.eg>
2023-11-14 23:53:17 +0200

    Added session 7

---
+++ OOP/G - Multiple Inheritance.py
@@ -0,0 +1,40 @@
+# ---------------------------------------------------------
+# -- Object Oriented Programming => Multiple Inheritance --
+# ---------------------------------------------------------
+
+class base1:
+
+    def __init__(self):
+
+        print("base1")
+
+    def fun1(self):
+
+        print("one")
+
+class base2:
+
+    def __init__(self):
+        
+        print("base2")
+
+    def fun2(self):
+
+        print("two")
+
+class derived(base1, base2):
+
+    pass
+
+var = derived()
+var.fun1() 
+var.fun2()
+
+
+
+
+
+
+
+
+
