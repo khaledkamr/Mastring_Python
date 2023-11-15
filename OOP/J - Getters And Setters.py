43b3edb21e681c08b30737c1844b40921339e275
khaledkamr <200013171@must.edu.eg>
2023-11-15 13:11:43 +0200

    Added notes

--- OOP/J - Getters And Setters.py
+++ OOP/J - Getters And Setters.py
@@ -1,7 +1,10 @@
 # -----------------------------------------------------
 # -- Object Oriented Programming => Getters & Setters--
 # -----------------------------------------------------
-
+# Assuming that incapsulation does really restrict the attributes
+# you can't get to the Private and Protected attributes
+# so here we can use the Getter and the Setter to help us in this
+ 
 class member:
 
     def __init__(self, name):
