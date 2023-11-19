333cb6ebf33cc4b70f278507bef5cb7f09494e11
khaledkamr <200013171@must.edu.eg>
2023-11-19 08:51:47 +0200

    updated the solution

--- Projects/Q - Probability Calculator.py
+++ Projects/Q - Probability Calculator.py
@@ -1,55 +1,49 @@
 from random import *
+from copy import *
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
+    count = 0
     for i in range(num_experiments):
 
-        balls = hat.draw(num_balls_drawn)
-        m = 0
+        expected_copy = deepcopy(expected_balls)
+        hat_copy = deepcopy(hat)
+        colors_gotten = hat_copy.draw(num_balls_drawn)
 
-        for ball in expected_balls:        
-            c = 0
-            if (ball in balls):
-                count = 0
-                for j in balls:
-                    if (j == ball):
-                        count += 1
+        for color in colors_gotten:
+            if(color in expected_copy):
+                expected_copy[color] -= 1
 
-                if(count == expected_balls[ball]):
-                    c += 1
-            
-            if(c == len(expected_balls)):
-                m += 1   
-
-    return m/num_experiments
+        if(all(x <= 0 for x in expected_copy.values())):
+            count += 1
+        
+    return count/num_experiments
 
 class Hat:
 
-    def __init__(self, *colors):
+    def __init__(self, **colors):
 
         self.contents = []
 
-        for color in colors:
-            c, i = color.split("=")
-            for j in range(int(i)):
-                self.contents.append(c)
+        for color, n in colors.items():
+            for i in range(n):
+                self.contents.append(color)
 
     def draw(self, n):
 
-        if n > len(self.contents):
+        if (n > len(self.contents)):
             return self.contents
 
         balls = []
-        size = len(self.contents) - 1
 
         for i in range(n):
+            size = len(self.contents) - 1
             drawnBall = self.contents[randint(0, size)]
             balls.append(drawnBall)
             self.contents.remove(drawnBall)
-            size -= 1
 
         return balls
 
-hat = Hat("black=6", "red=4", "green=3")
+hat = Hat(black=6, red=4, green=3)
 probability = experiment(hat, {"red":2,"green":1}, 5, 2000)
 print(probability)
