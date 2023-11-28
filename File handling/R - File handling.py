4ccfec9ee812ebf98c3b11f8dda7c7b3eeccfaa6
khaledkamr <200013171@must.edu.eg>
2023-11-28 19:26:30 +0200

    Added file hnadling

---
+++ File handling/R - File handling.py
@@ -0,0 +1,57 @@
+# -------------------
+# -- File Handling --
+# -------------------
+# "a" Append  Open File For Appending Values, Create File If Not Exists
+# "r" Read    [Default Value] Open File For Read and Give Error If File is Not Exists
+# "w" Write   Open File For Writing, Create File If Not Exists
+# "x" Create  Create File, Give Error If File Exists
+# --------------------------------------------------
+
+# import os
+
+# print(os.getcwd()) # Main current working directory
+# print(" ")
+
+# absPath = os.path.abspath(__file__) # the absolute path of the opened file
+# print(absPath) 
+# print(" ")
+
+# fileDirectory = os.path.dirname(absPath)
+# print(fileDirectory) # directory of the opened file
+# print(" ")
+
+# os.chdir(fileDirectory) # change current working directory
+# print(os.getcwd())
+# print(" ")
+
+# file = open(r"D:\files\khaled.txt") # r means this is raw string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
