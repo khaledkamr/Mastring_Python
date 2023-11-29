6e116471b0adcd7eed9beba70a1b5b3adde83d7f
khaledkamr <200013171@must.edu.eg>
2023-11-29 17:18:48 +0200

    Added session 3

---
+++ File handling/C - Write and append.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------
+# -- File Handling => Write and Append In File --
+# -----------------------------------------------
+
+myfile = open("D:\khaled.txt", "w")
+
+# the write mode overwrite the file
+myfile.write("hello from python\n")
+myfile.write("new line\n")
+
+myList = ["khaled\n", "yossif\n", "ahmed\n"]
+myfile.writelines(myList)
+
+# the append mode doesn't overwrite the file
+myfile = open("D:\khaled.txt", "a")
+myfile.write("hello from append")
+
+myfile.close()
