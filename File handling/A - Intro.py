641a4b0ed1d0d04a581f1fe65d7b2305ceea15b2
khaledkamr <200013171@must.edu.eg>
2023-12-20 22:36:07 +0200

    Added the binary mode

--- File handling/A - Intro.py
+++ File handling/A - Intro.py
@@ -2,10 +2,11 @@
 # -- File Handling --
 # -------------------
 # Modes:
-# "a" Append  Open File For Appending Values, Create File If Not Exists
-# "r" Read    [Default Value] Open File For Read and Give Error If File is Not Exists
-# "w" Write   Open File For Writing, Create File If Not Exists
-# "x" Create  Create File, Give Error If File Exists
+# "a" Append   Open File For Appending Values, Create File If Not Exists
+# "r" Read     [Default Value] Open File For Read and Give Error If File is Not Exists
+# "w" Write    Open File For Writing, Create File If Not Exists
+# "x" Create   Create File, Give Error If File Exists
+# "rb" Binary  return contents as bytes objects without any decoding
 # --------------------------------------------------
 
 # open() -> take two parameter, first one the file name, second is the mode,
